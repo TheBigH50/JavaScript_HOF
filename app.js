1e017b5916b463b6ec3d362b74e7d5d11c5982d6
TheBigH50 <HFangmeier@outlook.com>
2022-08-19 13:47:43 -0500

    Exe1 final

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ console.log("EXERCISE 1:\n==========\n");
 
 //Exe1
 
+let plus = function (plusNumber) {
+return function (number) {
+    return number + plusNumber;
+}
+};
+
+const plus15 = plus(15);
+
+console.log(plus15(10));
+
+//Exe2
