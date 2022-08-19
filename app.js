8d0067e8f2ad5315c14bbfbdf68eb2aea8a27e69
TheBigH50 <HFangmeier@outlook.com>
2022-08-19 13:48:09 -0500

    Exe2 paste in data

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ console.log("EXERCISE 1:\n==========\n");
 //Exe1
 
 let plus = function (plusNumber) {
-return function (number) {
+  return function (number) {
     return number + plusNumber;
-}
+  };
 };
 
 const plus15 = plus(15);
@@ -16,3 +16,30 @@ const plus15 = plus(15);
 console.log(plus15(10));
 
 //Exe2
+
+let users = [
+  {
+    name: "Frodo",
+    age: 50,
+    score: 85,
+    isActive: false,
+  },
+  {
+    name: "Sam",
+    age: 38,
+    score: 94,
+    isActive: true,
+  },
+  {
+    name: "Merry",
+    age: 36,
+    score: 82,
+    isActive: true,
+  },
+  {
+    name: "Pippin",
+    age: 26,
+    score: 77,
+    isActive: false,
+  },
+];
